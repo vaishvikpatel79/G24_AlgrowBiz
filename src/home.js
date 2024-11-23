d2828c48b9c91c3e233f7a56777cf1d9e02467b8
dp84 <122209357+devarshiPatel84@users.noreply.github.com>
2024-11-23 14:46:14 +0530

    updated home page

--- src/home.js
+++ src/home.js
@@ -1,21 +1,56 @@
-import React from "react";
-import { db } from "./firebase";
-import "./style.css";
+import React, { useEffect } from "react";
+// import "./style.css";
 import { Link, useNavigate } from 'react-router-dom';
-import { useState } from "react";
-import { auth } from "./firebase";
-
+import { useState} from "react";
+import axios from 'axios'; 
+import "./home.css";
 const Home_page = () => {
+    const userId = localStorage.getItem('userId');
+    const [userName, setUserName] = useState('');
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:5000/home/${userId}`);
+                if(response.data){
+                    setUserName(response.data);
+                }
+            } catch (err) {
+                console.error('An error occurred:', err);
+            }
+        };
+        fetchData();
+    }, [userId]); // Add userId as a dependency if it's being updated dynamically    
+
     return (
-        <div className='container'>
-            <div className='form'>
-                <h2>Welcome to Home Page</h2>
-                <button className='submit-btn'>Profile</button>
-                <button className='submit-btn'>Inventory Management</button>
-                <button className='submit-btn'>Forecast</button>
-                <button className='submit-btn'>Trends</button>
-            </div>
+        <div className="home-container">
+            <header className="home-header">
+                <h1>Welcome {userName !== '' ? {userName} : "to AlgrowBiz" } !</h1>
+                <p>Explore various features to manage your business.</p>
+            </header>
             
+            <div className="card-container">
+                {/* <Link to="/profile" className="card">
+                    <h3>Profile</h3>
+                    <p>View and edit your profile details.</p>
+                </Link> */}
+                <Link to="/forecast" className="card">
+                    <h3>Forecast</h3>
+                    <p>View sales forecasts and plan ahead.</p>
+                </Link>
+                <Link to="/inventory" className="card">
+                    <h3>Inventory</h3>
+                    <p>Manage your product inventory with ease.</p>
+                </Link>
+                <Link to="/inventoryOptimisation" className="card">
+                    <h3>Inventory Optimisation</h3>
+                    <p>Optimise your inventory with ease.</p>
+                </Link>
+                <Link to="/trends" className="card">
+                    <h3>Trends</h3>
+                    <p>Analyze the latest trends in your industry.</p>
+                </Link>
+            </div>
         </div>
     );
 };
